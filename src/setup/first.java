cb1f7bebdb3b8cb97f52f05c7f9b8e8fcb092f5b
17063 <sumeyrakaptan@gmail.com>
2021-10-09 17:13:10 -0400

    made the d method

--- src/setup/first.java
+++ src/setup/first.java
@@ -4,17 +4,6 @@ public class first {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        // This is a new line
-
-        // I want to create a history
-
-        //The text doesn't matter
-
-        //third commit
-
-        //more lines
-
-
 
 
     }
